4a79999a30dab7077b2977d8db028227f166a197
LadyKillerr <dungvuduc0509@gmail.com>
2023-09-29 13:43:42 +0700

    refactor: fixed some conflict in bullet logic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,25 +5,16 @@ using UnityEngine.InputSystem;
 
 public class Bullet : MonoBehaviour
 {
-<<<<<<< Updated upstream
-=======
-    [SerializeField] float bulletSpeed = 15;
-    Rigidbody2D bulletRigidbody;
-    Vector2 bulletTrajectory;
->>>>>>> Stashed changes
-
     [SerializeField] float bulletSpeed = 15;
 
     Rigidbody2D bulletRigidbody;
     Transform bulletTransform;
-<<<<<<< Updated upstream
 
-    float bulletTrajectory;
-    float bulletDirection;
 
-=======
+    Vector2 bulletTrajectory;
+
     Vector2 bulletDirection;
->>>>>>> Stashed changes
+
     // get ra player 
     PlayerMovement myPlayer;
 
@@ -40,44 +31,31 @@ public class Bullet : MonoBehaviour
         // Get ra player ở một script playermovement
         myPlayer = FindObjectOfType<PlayerMovement>();
 
-<<<<<<< Updated upstream
-        // Hướng đạn bay dựa theo ng chơi
-        bulletTrajectory = myPlayer.transform.localScale.x * bulletSpeed;
-
-        // Mặt Sprite viên đạn dựa theo ng chơi
-        bulletDirection = myPlayer.transform.localScale.x;
-=======
-
         // Làm đường đạn
         bulletTrajectory = new (myPlayer.transform.localScale.x * bulletSpeed, 0f);
+
         // Làm hướng của viên đạn
         bulletDirection = myPlayer.transform.localScale;
->>>>>>> Stashed changes
+
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+
         BulletHeading();
     }
 
 
     void BulletHeading()
     {
-        // Hướng bay viên đạn
-        bulletRigidbody.velocity = new (bulletTrajectory, 0f);
-
-        // Mặt Sprite viên đạn
-        bulletTransform.localScale = new Vector3(bulletDirection, 1, 1);
-=======
         BulletFiring();
     }
+
     void BulletFiring()
     {
         bulletRigidbody.velocity = bulletTrajectory; // bắn đạn ra
         bulletTransform.localScale = bulletDirection; // đạn quay hướng nào
->>>>>>> Stashed changes
     }
 }
 
