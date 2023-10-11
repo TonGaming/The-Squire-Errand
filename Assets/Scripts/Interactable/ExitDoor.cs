4b9492fbe82684d02f0afbd9b470454de872066d
LadyKillerr <dungvuduc0509@gmail.com>
2023-10-11 12:31:47 +0700

    feat: fixed the exit door logic

--- Assets/Scripts/Interactable/ExitDoor.cs
+++ Assets/Scripts/Interactable/ExitDoor.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -29,23 +29,21 @@ public class ExitDoor : MonoBehaviour
         }
     }
 
-
-
     IEnumerator LoadNextLevel()
     {
         yield return new WaitForSecondsRealtime(LevelLoadDelay); // current load delay is 2 seconds
 
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // lấy ra index của scene hiện tại
 
-        int nextSceneIndex = currentSceneIndex + 1;
+        int nextSceneIndex = currentSceneIndex + 1;  // index của scene tiếp theo sẽ bằng scene hiện tại + 1
 
 
-        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) // nếu index scene tiếp bằng tổng số lượng scene trong build setting, chẳng hạn có 5 levels mà nextSceneIndex = 5 -> thì reset lại về 0
         {
-            nextSceneIndex = 0;
+            nextSceneIndex = 0; // khi reset lại scene index = 0 thì sẽ load lại map đầu tiên 
         }
 
-        SceneManager.LoadScene(nextSceneIndex);
+        SceneManager.LoadScene(nextSceneIndex); // Loadlevel kế dựa trên currentSceneIndex + 1
 
     }
 }
