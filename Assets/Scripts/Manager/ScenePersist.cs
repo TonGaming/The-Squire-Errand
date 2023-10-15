cf0d8983a18a84f05107ff0262c8120eeafcb547
Lady Killerr <dungvuduc0509@gmail.com>
2023-10-15 15:45:22 +0700

    feat: re design level2

---
+++ Assets/Scripts/Manager/ScenePersist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePersist : MonoBehaviour
+{
+    void Awake()
+    {
+        int numScenePersists = FindObjectsOfType<GameSession>().Length;
+
+        if (numScenePersists > 1)
+        {
+            Destroy(gameObject); // check xem nếu có nhiều ScenePersist thì xoá cái này đi 
+
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject); // nếu chỉ có một thì giữ nguyên
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
